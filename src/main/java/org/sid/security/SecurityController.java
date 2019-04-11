7e36b3c7340a6567f9e1ec7707e7c697358b8f3a
anelmkoudi <anass.el_mkoudi@etu.uca.fr>
2019-04-11 23:16:29 +0200

    resolution du probleme de redirection par defaut /

--- src/main/java/org/sid/security/SecurityController.java
+++ src/main/java/org/sid/security/SecurityController.java
@@ -1,18 +1,20 @@
 package org.sid.security;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class SecurityController {
 
-	 /*@RequestMapping(value="/login")
+	/* @RequestMapping(value="/login")
 	  public String login() {
 		  return "login";
-	  }
-	  */
-	  /*@RequestMapping(value="/")
-	  public String home() {
-		  return "redirect:/consulterCompte";
 	  }*/
+	  
+	  @RequestMapping(value="/")
+	  public String home() {
+		  return "redirect:/login";
+	  }
 	  
 	  /*@RequestMapping(value="/403")
 	  public String accessDenied() {
